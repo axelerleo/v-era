fc28fdbbdc366257e64b406638233cc885d8a80a
Alexey <axelerleo@yandex.ru>
2019-01-18 23:20:14 +0300

    added up/down button

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,63 @@ $(".main-parallax").each(function() {
     }
   });
 });
+
+/* begin Up-Down button  */
+(function() {
+  "use strict";
+
+  var upDownBtn = document.querySelector(".up_down_btn");
+  var check;
+  var coordsY = 0;
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      upDownBtn.classList.add("up_down_btn-show");
+      upDownBtn.innerHTML = "&uarr;";
+      upDownBtn.setAttribute("title", "Наверх");
+      check = false;
+    }
+    if (scrolled === 0) {
+      upDownBtn.innerHTML = "&darr;";
+      upDownBtn.setAttribute("title", "Вниз");
+      check = true;
+    }
+  }
+
+  function backToTop() {
+    upDownBtn.classList.add("up_down_btn-disabled");
+
+    if (!check) {
+      coordsY = pageYOffset;
+      (function goTop() {
+        if (window.pageYOffset !== 0) {
+          window.scrollBy(0, -30);
+          setTimeout(goTop, 0);
+        } else {
+          upDownBtn.classList.remove("up_down_btn-disabled");
+        }
+      })();
+      return;
+    } else if (check) {
+      (function goBottom() {
+        var match = Math.ceil(Math.min(coordsY, pageYOffset));
+        console.log("coordsY " + coordsY);
+        console.log("current " + match);
+        if (match < coordsY) {
+          window.scrollBy(0, Math.min(coordsY - match, 30));
+          setTimeout(goBottom, 0);
+        } else {
+          upDownBtn.classList.remove("up_down_btn-disabled");
+        }
+      })();
+      return;
+    }
+  }
+
+  window.addEventListener("scroll", trackScroll);
+  upDownBtn.addEventListener("click", backToTop);
+})();
+/* end Up-Down button  */
