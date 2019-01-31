bb8f1a55829f1c36b0fb2102decc5d0b1e6ed471
Alexey <axelerleo@yandex.ru>
2019-01-31 20:30:42 +0300

    scroll effects

--- js/scripts.js
+++ js/scripts.js
@@ -271,27 +271,36 @@ $(function() {
   });
 });
 
+// $(function() {
+//   var $window = $(window); //Window object
+
+//   var scrollTime = 1.2; //Scroll time
+//   var scrollDistance = 300; //Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+//   $window.on("mousewheel DOMMouseScroll", function(event) {
+//     event.preventDefault();
+
+//     var delta =
+//       event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+//     var scrollTop = $window.scrollTop();
+//     var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+//     TweenMax.to($window, scrollTime, {
+//       scrollTo: { y: finalScroll, autoKill: true },
+//       ease: Power1.easeOut, //For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+//       autoKill: true,
+//       overwrite: 5
+//     });
+//   });
+// });
+
+// $(function() {
+//   $("html").niceScroll({ smoothscroll: true });
+// });
+
 $(function() {
-  var $window = $(window); //Window object
-
-  var scrollTime = 1.2; //Scroll time
-  var scrollDistance = 300; //Distance. Use smaller value for shorter scroll and greater value for longer scroll
-
-  $window.on("mousewheel DOMMouseScroll", function(event) {
-    event.preventDefault();
-
-    var delta =
-      event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
-    var scrollTop = $window.scrollTop();
-    var finalScroll = scrollTop - parseInt(delta * scrollDistance);
-
-    TweenMax.to($window, scrollTime, {
-      scrollTo: { y: finalScroll, autoKill: true },
-      ease: Power1.easeOut, //For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
-      autoKill: true,
-      overwrite: 5
-    });
-  });
+  // Custom Easing
+  jQuery.scrollSpeed(200, 1200, "easeOutSine");
 });
 
 window.addEventListener("load", function() {
